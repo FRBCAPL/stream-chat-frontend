b4aa8e1fbf20030df049dc0d4c0a3d18631dc79f
frbcapl@gmail.com <frbcapl@gmail.com>
2025-05-09 04:10:44 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/stream-chat-frontend/',
+export default defineConfig({
+  base: '/stream-chat-frontend/',
   plugins: [react()],
-}));
-
+});
